c7a2503bfeaab98c6406e5a665990956219252a4
Uwe Mayer <mayer@software-driven.de>
2023-03-18 15:41:38 +0100

    another example

--- custom_components/tylo/__init__.py
+++ custom_components/tylo/__init__.py
@@ -1,31 +1,36 @@
-"""
-The "hello world" custom component.
-
-This component implements the bare minimum that a component should implement.
-
-Configuration:
-
-To use the hello_world component you will need to add the following to your
-configuration.yaml file.
-
-hello_world_async:
-"""
+"""The Detailed Hello World Push integration."""
 from __future__ import annotations
 
-import asyncio
-
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
-# The domain of your component. Should be equal to the name of your component.
-DOMAIN = "hello_world_async"
+from . import hub
+from .const import DOMAIN
+
+# List of platforms to support. There should be a matching .py file for each,
+# eg <cover.py> and <sensor.py>
+PLATFORMS: list[str] = ["cover", "sensor"]
 
 
-@asyncio.coroutine
-def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Setup our skeleton component."""
-    # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.async_set('hello_world_async.Hello_World', 'Works!')
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Hello World from a config entry."""
+    # Store an instance of the "connecting" class that does the work of speaking
+    # with your actual devices.
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = hub.Hub(hass, entry.data["host"])
 
-    # Return boolean to indicate that initialization was successfully.
+    # This creates each HA object for each platform your device requires.
+    # It's done by calling the `async_setup_entry` function in each platform module.
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    # This is called when an entry/configured device is to be removed. The class
+    # needs to unload itself, and remove callbacks. See the classes for further
+    # details
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
